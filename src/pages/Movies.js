1bde30d9ecb8ee7228ccb0e87e5ccc5cf9f2d794
unknown <acsgyorgy.mail@gmail.com>
2022-10-31 13:44:40 +0100

    search and the movies page are swapped

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,35 +1,31 @@
 import '../css/PagesStyle.css';
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import Header from '../components/Header.js';
 import Footer from '../components/Footer.js';
 import { useNavigate } from "react-router-dom";
-
 
 const Movies = () => {
 
     let navigate = useNavigate();
 
-    const [allMoviesType, setAllMoviesType] = useState([]);
-    const [searchForMovies, setSearchForMovies] = useState("");
-
+    const allMovies = useRef([]);
+    const [visibleMovies, setVisibleMovies] = useState([]);
+    const [moviesTitle, setMoviesTitle] = useState("");
 
     useEffect (() => {
         fetch("http://localhost:4000/all-movies")
                 .then(data => data.json())
                 .then(parsedData => {
-                    setAllMoviesType(parsedData);
+                  allMovies.current = parsedData;
+                  setVisibleMovies(parsedData);
                 })
-    }, [])
-
-
-
-    const filterByAllMoviesType = () => {
-        fetch("http://localhost:4000/all-movies-type/" + searchForMovies)
-                .then(data => data.json())
-                .then(parsedData => {
-                    setAllMoviesType(parsedData);
-                })
-    };
+      }, [])
+      
+      //Frontend filter 
+      const filterByMoviesTitle = () => {
+        const filteredMovies = allMovies.current.filter(movie => movie.movieMainTitle.includes(moviesTitle));
+        setVisibleMovies(filteredMovies);
+    }
 
     return (
         <div className="PagesContainer">
@@ -37,51 +33,24 @@ const Movies = () => {
                 <Header/>
             </div>
             <div>
-                <div className="SelectsForFilter">
+                <div>
+                    Search by name: 
                     <div>
-                        <select>
-                            <option>Movie type</option>
-                            <option>Action</option>
-                            <option>Scifi</option>
-                            <option>Fantasy</option>
-                        </select>
+                        <input onChange={(e) => setMoviesTitle(e.target.value)} value={moviesTitle}/>
+                            <button onClick={filterByMoviesTitle}>Filter movies by titles</button>
+                                {
+                                    visibleMovies.map( movies => (
+                                        <div >  
+                                            <img 
+                                                src={"http://localhost:4000/icons/" + movies.icon} 
+                                                style={{width: "200px", marginRight: "20px"}} 
+                                                alt="moviepicture"
+                                                onClick={() => {navigate("/selectedmovie")}}
+                                                />
+                                        </div>
+                                    ))
+                                }
                     </div>
-                    <div>
-                        <select>
-                            <option>Search by year</option>
-                            <option>2010-2022</option>
-                            <option>2000-2010</option>
-                            <option>1990-2000</option>
-                            <option>1980-1990</option>
-                        </select>
-                    </div>
-                    <div>
-                        <select>
-                            <option>Search by movie time</option>
-                            <option>more than 2 hours</option>
-                            <option>under than 2 hours</option>
-                            <option>between 1 and 2 hours</option>
-                            <option>under than 1 hour</option>
-                        </select>
-                    </div>  
-                    <div>
-                        <input onChange={(e) => setSearchForMovies(e.target.value)} value={searchForMovies}/>
-                        <button onClick={filterByAllMoviesType}>Filter movies type pl.: action, fantasy</button>
-                    </div> 
-                    <div>
-                        {
-                            allMoviesType.map( moviesType => (
-                                <div >  
-                                    <img 
-                                        src={"http://localhost:4000/icons/" + moviesType.icon} 
-                                        style={{width: "200px", marginRight: "20px"}} 
-                                        alt="moviepicture"
-                                        onClick={() => {navigate("/selectedmovie")}}
-                                    />
-                                </div>
-                            ))
-                        }
-                    </div>        
                 </div>
             </div>
             <div>
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,30 +1,49 @@
 import '../css/PagesStyle.css';
-import React, {useState, useEffect, useRef} from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from '../components/Header.js';
 import Footer from '../components/Footer.js';
-import { useNavigate } from "react-router-dom";
+//import { useNavigate } from "react-router-dom";
+import { useContext } from 'react';
+import { MovieContext } from "../context/MovieContext.js";
 
-const Search = () => {
 
-    let navigate = useNavigate();
 
-    const allMovies = useRef([]);
+const Search = () => { 
+
+    //let navigate = useNavigate();
+
+    const movieDetails = useContext(MovieContext);
+
+    const [allMoviesType, setAllMoviesType] = useState([]);
+    const [searchForMovies, setSearchForMovies] = useState("");
     const [visibleMovies, setVisibleMovies] = useState([]);
-    const [moviesTitle, setMoviesTitle] = useState("");
+
 
     useEffect (() => {
         fetch("http://localhost:4000/all-movies")
                 .then(data => data.json())
                 .then(parsedData => {
-                  allMovies.current = parsedData;
-                  setVisibleMovies(parsedData);
+                    setAllMoviesType(parsedData);
                 })
-      }, [])
-      
-      //Frontend filter 
-      const filterByMoviesTitle = () => {
-        const filteredMovies = allMovies.current.filter(movie => movie.movieMainTitle.includes(moviesTitle));
-        setVisibleMovies(filteredMovies);
+    }, [])
+
+
+
+
+    const filterByAllMoviesType = () => {
+        fetch("http://localhost:4000/all-movies-type/" + searchForMovies)
+                .then(data => data.json())
+                .then(parsedData => {
+                    setAllMoviesType(parsedData);
+                })
+    };
+
+    const miez = () => {
+        fetch("http://localhost:4000/all-movies-by-action/action")
+            .then(data => data.json())
+            .then(parsedData => {
+                setVisibleMovies(parsedData)
+            })
     }
 
     return (
@@ -33,25 +52,55 @@ const Search = () => {
                 <Header/>
             </div>
             <div>
-                <div>
-                    Search by name: 
+                <div className="SelectsForFilter">
                     <div>
-                        <input onChange={(e) => setMoviesTitle(e.target.value)} value={moviesTitle}/>
-                            <button onClick={filterByMoviesTitle}>Filter movies by titles</button>
-                                {
-                                    visibleMovies.map( movies => (
-                                        <div >  
-                                            <img 
-                                                src={"http://localhost:4000/icons/" + movies.icon} 
-                                                style={{width: "200px", marginRight: "20px"}} 
-                                                alt="moviepicture"
-                                                onClick={() => {navigate("/selectedmovie")}}
-                                                />
-                                        </div>
-                                    ))
-                                }
+                        <select>
+                            <option>Movie type</option>
+                            <option onClick={miez}>Action</option>
+                            <option>Scifi</option>
+                            <option>Fantasy</option>
+                        </select>
                     </div>
+                    <div>
+                        <select>
+                            <option>Search by year</option>
+                            <option>2010-2022</option>
+                            <option>2000-2010</option>
+                            <option>1990-2000</option>
+                            <option>1980-1990</option>
+                        </select>
+                    </div>
+                    <div>
+                        <select>
+                            <option>Search by movie time</option>
+                            <option>more than 2 hours</option>
+                            <option>under than 2 hours</option>
+                            <option>between 1 and 2 hours</option>
+                            <option>under than 1 hour</option>
+                        </select>
+                    </div>  
+                    <div>
+                        <input onChange={(e) => setSearchForMovies(e.target.value)} value={searchForMovies}/>
+                        <button onClick={filterByAllMoviesType}>Filter movies type pl.: action, fantasy</button>
+                    </div>       
                 </div>
+            
+                <div>               
+                        {
+                            visibleMovies.map( movies => (
+                                <div >  
+                                    <img 
+                                        src={"http://localhost:4000/icons/" + movies.icon} 
+                                        style={{width: "200px", marginRight: "20px"}} 
+                                        alt="moviepicture"
+                                    />
+                                </div>
+                            ))
+                        }
+                </div>
+                    
+                    <div>
+                    </div>
             </div>
             <div>
                 <Footer/>
