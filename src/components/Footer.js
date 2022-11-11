7a5405a195174e2e9863928b817dd8123b3c7f7e
unknown <acsgyorgy.mail@gmail.com>
2022-11-11 13:34:15 +0100

    footer done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,35 @@
 import '../css/Footer.css'
 import React from 'react';
 
-const Footer = () => { 
+const Footer = () => {  
      return (
         <div className="Footer">
-            <div className="col">
-              <p>Privacy Policy</p>
+            <div className="col footer-logo">
+                MOVIE+
             </div>
+                <div className="footer-menu">
+                    <div className="col">
+                        <p>Privacy Policy</p>
+                    </div>
+                    <div className="col">
+                        <p>Cookie Policy</p>
+                    </div>
+                    <div className="col">
+                        <p>Privacy Rights(EU and UK)</p>
+                    </div>
+                    <div className="col">
+                        <p>Help</p>
+                    </div>
+                    <div className="col">
+                        <p>Supported Devices</p>
+                    </div>
+                    <div className="col">
+                        <p>About Us</p>
+                    </div>
+                </div>
             <div className="col">
-              <p>Cookie Policy</p>
-            </div>
-            <div className="col">
-              <p>Help</p>
-            </div>
-            <div className="col">
-              <p>About Us</p>
-            </div>
+                <p>&copy; movie+</p>
+            </div>    
         </div>
   )
 }
