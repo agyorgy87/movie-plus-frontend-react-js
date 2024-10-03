fd4a6b97caee4bda6cbdf69f900b50e6d12f8ed3
unknown <acsgyorgy.mail@gmail.com>
2024-10-03 12:41:20 +0200

    Collections component added to component folder. Everything what need from the Home page has been moved to the Collections component.

---
+++ src/components/Collections.js
@@ -0,0 +1,44 @@
+import React, {useState, useEffect, useRef} from 'react';
+import { useNavigate } from "react-router-dom";
+import { useContext } from 'react';
+import { CollectionMovieContext } from "../context/CollectionMovieContext.js";
+import axios from "axios";
+
+const Collections = () => {
+
+    let navigate = useNavigate();
+
+    const [collectionMovies, setCollectionMovies] = useState([]); 
+
+    const collectionMovieDetails = useContext(CollectionMovieContext);
+
+    const baseUrlForImages = process.env.REACT_APP_API_URL || 'http://localhost:4000';
+
+    const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:4000';
+
+    useEffect(() => {  
+        axios.get(`${baseURL}/collection-movies`)
+            .then(response => setCollectionMovies(response.data));     
+    }, [])
+
+    return (
+        <div>
+            <h2 className="movie-genre-texts">Collections</h2>                       
+                <div className="home-movies-container">
+                    {
+                        collectionMovies.map((movies,index) => (
+                            <div key={index}>  
+                                <img                                               
+                                src={`${baseUrlForImages}/collection-icons/${movies.collectionIcon}`} 
+                                className="home-movie-icons" 
+                                alt="moviepicture"
+                                onClick={() => { collectionMovieDetails.setValue(movies); navigate("/selectedcollectionmovie")}}/>
+                            </div>
+                        ))
+                    }
+                </div>
+        </div>
+    )
+}
+
+export default Collections;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,74 +5,51 @@ import Slider from "../components/Slider.js";
 import ActionMoviesSlider from '../components/ActionMoviesSlider.js';
 import ComedyMoviesSlider from '../components/ComedyMoviesSlider.js';
 import SciFiMoviesSlider from '../components/SciFiMoviesSlider.js';
+import Collections from '../components/Collections.js';
 import Footer from "../components/Footer.js";
-import React, {useState, useEffect, useRef} from 'react';
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import React from 'react';
 import { useContext } from 'react';
 import { MovieContext } from "../context/MovieContext.js";
-import { CollectionMovieContext } from "../context/CollectionMovieContext.js" 
+
+
 
 const Home = () => {
 
-    let navigate = useNavigate();
-
+    /*
     const [allMovies, setAllMovies] = useState([]);
     
-    const [collectionMovies, setCollectionMovies] = useState([])    
-
-    const movieDetails = useContext(MovieContext);
-    const collectionMovieDetails = useContext(CollectionMovieContext);
-
-    const baseUrlForImages = process.env.REACT_APP_API_URL || 'http://localhost:4000';
-
     const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:4000';
 
     useEffect(() => {  
-
         axios.get(`${baseURL}/all-movies`)
             .then(response => setAllMovies(response.data));
-
-        axios.get(`${baseURL}/collection-movies`)
-            .then(response => setCollectionMovies(response.data));     
     }, [])
+    */
+
+    const movieDetails = useContext(MovieContext);
 
     return (  
         <div className="pages-container"> 
             <div className="header">
                 <NavigationBar/>
             </div>            
-                <div> 
-                    <div className="row">
-                        <Slider/>
-                    </div>
-                    <div className="row">
-                        <ActionMoviesSlider movieDetails={movieDetails}/>
-                    </div>
-                    <div className="row">
-                        <ComedyMoviesSlider movieDetails={movieDetails}/>        
-                    </div>
-                    <div className="row">
-                        <SciFiMoviesSlider movieDetails={movieDetails}/>
-                    </div>
-                    <div className="row">  
-                        <h2 className="movie-genre-texts">Collections</h2>                       
-                            <div className="home-movies-container">
-                                {
-                                    collectionMovies.map((movies,index) => (
-                                        <div key={index}>  
-                                            <img                                               
-                                                src={`${baseUrlForImages}/collection-icons/${movies.collectionIcon}`} 
-                                                className="home-movie-icons" 
-                                                alt="moviepicture"
-                                                onClick={() => { collectionMovieDetails.setValue(movies); navigate("/selectedcollectionmovie")}}
-                                            />
-                                        </div>
-                                    ))
-                                }
-                            </div>
-                    </div>               
+            <div> 
+                <div className="row">
+                    <Slider/>
                 </div>
+                <div className="row">
+                    <ActionMoviesSlider movieDetails={movieDetails}/>
+                </div>
+                <div className="row">
+                    <ComedyMoviesSlider movieDetails={movieDetails}/>        
+                </div>
+                <div className="row">
+                    <SciFiMoviesSlider movieDetails={movieDetails}/>
+                </div>
+                <div className="row">  
+                    <Collections/>
+                </div>               
+            </div>
             <div className="footer">
                 <Footer/>
             </div>
@@ -82,4 +59,4 @@ const Home = () => {
 
 export default Home;
 
-/* 354 line */
+/* 354 line -> 60*/
