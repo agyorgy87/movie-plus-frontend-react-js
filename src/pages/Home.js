75ddfeb9269ab03411c1449f47d8ee518f6d441b
unknown <acsgyorgy.mail@gmail.com>
2024-10-03 12:30:39 +0200

    SciFiMoviesSlider component added to component folder. Everything what need from the Home page has been moved to the SciFiMoviesSlider component.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import NavigationBar from "../components/NavigationBar.js";
 import Slider from "../components/Slider.js";
 import ActionMoviesSlider from '../components/ActionMoviesSlider.js';
 import ComedyMoviesSlider from '../components/ComedyMoviesSlider.js';
+import SciFiMoviesSlider from '../components/SciFiMoviesSlider.js';
 import Footer from "../components/Footer.js";
 import React, {useState, useEffect, useRef} from 'react';
 import axios from "axios";
@@ -11,91 +12,30 @@ import { useNavigate } from "react-router-dom";
 import { useContext } from 'react';
 import { MovieContext } from "../context/MovieContext.js";
 import { CollectionMovieContext } from "../context/CollectionMovieContext.js" 
-import { MdArrowBackIos, MdArrowForwardIos} from 'react-icons/md';
-
 
 const Home = () => {
-
-    let scifiMoviesScrl = useRef(null);
 
     let navigate = useNavigate();
 
     const [allMovies, setAllMovies] = useState([]);
     
-    const [scifiMovies, setScifiMovies] = useState([]);
     const [collectionMovies, setCollectionMovies] = useState([])    
-
-    
-
-    const [scifiMoviesScrollX, setScifiMoviesScrollX] = useState(0);
-    const [scifiMoviesScrollEnd, setScifiMoviesScrollEnd] = useState(false);
 
     const movieDetails = useContext(MovieContext);
     const collectionMovieDetails = useContext(CollectionMovieContext);
 
     const baseUrlForImages = process.env.REACT_APP_API_URL || 'http://localhost:4000';
 
+    const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:4000';
+
     useEffect(() => {  
-
-        const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:4000';
-
-        //axios.get(`${baseURL}/slide-show-images`)
-        //.then(response => setSliderData(response.data));
 
         axios.get(`${baseURL}/all-movies`)
             .then(response => setAllMovies(response.data));
 
-        axios.get(`${baseURL}/all-movies-by-scifi/scienceFiction`)
-            .then(response => setScifiMovies(response.data));
-
         axios.get(`${baseURL}/collection-movies`)
             .then(response => setCollectionMovies(response.data));     
     }, [])
-    
-      /*scifi movies slider useeffect, slide and scrollcheckfunctions*/
-
-    useEffect(() => {
-        if (
-          scifiMoviesScrl.current &&
-          scifiMoviesScrl?.current?.scrollWidth === scifiMoviesScrl?.current?.offsetWidth
-        ) {
-          setScifiMoviesScrollEnd(true);
-        } else {
-          setScifiMoviesScrollEnd(false);
-        }
-        return () => {};
-    }, [scifiMoviesScrl?.current?.scrollWidth, scifiMoviesScrl?.current?.offsetWidth]);
-    
-
-    const scifiMoviesSlide = (shift) => {
-        scifiMoviesScrl.current.scrollLeft += shift;
-        setScifiMoviesScrollX(scifiMoviesScrollX + shift); 
-    
-        if (
-          Math.floor(scifiMoviesScrl.current.scrollWidth - scifiMoviesScrl.current.scrollLeft) <=
-          scifiMoviesScrl.current.offsetWidth
-        ) {
-          setScifiMoviesScrollEnd(true);
-        } else {
-          setScifiMoviesScrollEnd(false);
-        }
-      };
-
-    const scifiMoviesScrollCheck = () => {
-        setScifiMoviesScrollX(scifiMoviesScrl.current.scrollLeft);
-        if (
-          Math.floor(scifiMoviesScrl.current.scrollWidth - scifiMoviesScrl.current.scrollLeft) <=
-          scifiMoviesScrl.current.offsetWidth
-        ) {
-          setScifiMoviesScrollEnd(true);
-        } else {
-          setScifiMoviesScrollEnd(false);
-        }
-      };
-
-    let iconSize = parseInt(window.getComputedStyle(document.getElementsByTagName("html")[0]).fontSize.split("px")[0]) * 20
-    let iconSizeWithMargin = iconSize + 33.6
-
 
     return (  
         <div className="pages-container"> 
@@ -113,41 +53,7 @@ const Home = () => {
                         <ComedyMoviesSlider movieDetails={movieDetails}/>        
                     </div>
                     <div className="row">
-                        <div>
-                            <h2 className="movie-genre-texts">Science fiction movies</h2>
-                        </div>
-                        <div className="different-genres-containers">
-                            <div className="home-movies-container" ref={scifiMoviesScrl} onScroll={scifiMoviesScrollCheck}>
-                                {
-                                    scifiMovies.map((movies,index) => (
-                                        <div key={index}>  
-                                            <img 
-                                                src={`${baseUrlForImages}/icons/${movies.icon}`}  
-                                                className="home-movie-icons" 
-                                                alt="moviepicture"
-                                                onClick={() => { movieDetails.setValue(movies); navigate("/selectedmovie")}}
-                                            />
-                                        </div>
-                                    ))
-                                }
-                            </div>
-                            <div className="prevoius-arrow-container">
-                                {scifiMoviesScrollX !== 0 && (                           
-                                    <MdArrowBackIos
-                                    className="prev-button" 
-                                    onClick={() => scifiMoviesSlide(- iconSizeWithMargin)}                                   
-                                    />                                                     
-                                )}
-                            </div>
-                            <div className="next-arrow-container">
-                                {!scifiMoviesScrollEnd && (
-                                    <MdArrowForwardIos
-                                    className="next-button" 
-                                    onClick={() => scifiMoviesSlide(iconSizeWithMargin)}                              
-                                    />
-                                )}
-                            </div> 
-                        </div>
+                        <SciFiMoviesSlider movieDetails={movieDetails}/>
                     </div>
                     <div className="row">  
                         <h2 className="movie-genre-texts">Collections</h2>                       
